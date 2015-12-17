b525c7a765c4d8df44e2f23c72bebd812c0ff045
Martin Sidaway <msidaway@gmail.com>
2015-12-17 13:46:02 +0000

    Improve whitespace.

--- data/modify-page.js
+++ data/modify-page.js
@@ -1,51 +1,51 @@
 katakanaReceived = function(katakanaStr) {
 
-var converter = new K2RConverter(katakanaStr);
+    var converter = new K2RConverter(katakanaStr);
 
-var html = document.body.innerHTML;
+    var html = document.body.innerHTML;
 
-var kata = '\u30A1-\u30F5\u30F7-\u30FB\u31F0-\u31FF\uFF65-\uFF9D';
-var regexp = new RegExp('['+kata+']['+kata+'\u30FC\u309A]*', 'g');
+    var kata = '\u30A1-\u30F5\u30F7-\u30FB\u31F0-\u31FF\uFF65-\uFF9D';
+    var regexp = new RegExp('['+kata+']['+kata+'\u30FC\u309A]*', 'g');
 
-var pos = 0;
-var result = "";
-var found = false;
-var matches;
-var inTag = false;
+    var pos = 0;
+    var result = "";
+    var found = false;
+    var matches;
+    var inTag = false;
 
-while ((matches = regexp.exec(html)) !== null) {
-    var intermediate = html.substr(pos, matches.index - pos);
-    result += intermediate;
-    pos = regexp.lastIndex;
-    k = matches[0];
+    while ((matches = regexp.exec(html)) !== null) {
+        var intermediate = html.substr(pos, matches.index - pos);
+        result += intermediate;
+        pos = regexp.lastIndex;
+        k = matches[0];
 
-    var oti = intermediate.lastIndexOf('<');
-    var cti = intermediate.lastIndexOf('>');
-    if (oti < cti) {
-        inTag = false;
-    } else if (cti < oti) {
-        inTag = true;
+        var oti = intermediate.lastIndexOf('<');
+        var cti = intermediate.lastIndexOf('>');
+        if (oti < cti) {
+            inTag = false;
+        } else if (cti < oti) {
+            inTag = true;
+        }
+
+        if (inTag) {
+            result += k;
+            continue;
+        }
+
+        found = true;
+
+        r = converter.convert(k);
+
+        r = '<span title="'+ k +'">' + r + '</span>';
+        r = ' ' + r + ' ';
+        result += r;
     }
 
-    if (inTag) {
-        result += k;
-        continue;
+    result += html.substr(pos);
+
+    if (found) {
+        document.body.innerHTML = result;
     }
-
-    found = true;
-
-    r = converter.convert(k);
-
-    r = '<span title="'+ k +'">' + r + '</span>';
-    r = ' ' + r + ' ';
-    result += r;
-}
-
-result += html.substr(pos);
-
-if (found) {
-    document.body.innerHTML = result;
-}
 
 }; // katakanaReceived = function (katakana) { ...
 
